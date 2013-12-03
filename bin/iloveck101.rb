923855e9e9d8dd3ee19021f157d2f1decd305738
shunwen <hsiaoshunwen@gmail.com>
2013-12-04 01:09:26 +0800

    Workable version

--- bin/iloveck101.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-require 'iloveck101'
-Iloveck101.love ARGV[0]
--- lib/iloveck101.rb
+++ lib/iloveck101.rb
@@ -3,30 +3,37 @@ require "open-uri"
 require "mini_magick"
 
 module Iloveck101
+
   def self.love uri, w_min=400, h_min=400
-    html = Nokogiri::HTML uri.read, nil, 'UTF-8'
+    html = Nokogiri::HTML open(uri), nil, 'UTF-8'
+
     thread_id = uri.match(/thread-(\d+)-.*/)[1]
     title = html.xpath('.//title').text.split(' - ')[0].gsub('/','').strip
-
-    raise "Fail to love uri: #{uri}" unless thread_id and title
 
     folder = File.join Dir.home, 'Pictures/iloveck101', "#{thread_id} - #{title}"
     FileUtils.mkdir_p folder unless Dir.exist? folder
 
-    html.xpath('//img/@file').uniq.each do |img_file|
-      next unless img_file.value.start_with? 'http'
-      img_url = URI.parse img_file.value
-      filename = img_url.path.match(/\/(.*)/)[1]
-      print "Fetching #{img.value} ..."
-      i = MiniMagick::Image.open img.value
-      if i[:width] < w_min or i[:height] < h_min
-        print "Skipped, size too small\n"
-      else
-        i.write File.join folder, filename
-        print "Done!\n"
+    html.xpath('//img/@file').each do |img_file|
+      begin
+        next unless img_file.value.start_with? 'http'
+        filename = img_file.value.split('/').last
+        print "Fetching #{filename} ..."
+
+        img = MiniMagick::Image.open img_file.value
+
+        if img[:width] < w_min or img[:height] < h_min
+          print "Skip: Image too small!\n"
+        else
+          img.write File.join(folder, filename)
+          print "Done!\n"
+        end
+
+      rescue => e
+        puts "Failed to fetch #{img_file.value}: #{e.message}"
+        next
       end
-    rescue
-      next
     end
+
   end
+
 end
